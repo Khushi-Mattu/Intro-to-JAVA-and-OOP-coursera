ab9d061a81be1f8cfc6a77ffc53d29719fca1ac2
Khushi Mattu <82451914+Khushi-Mattu@users.noreply.github.com>
2022-01-24 10:26:38 +0530

    CustomerClassProject_w_toString

---
+++ WEEK 2/CustomerClassProject_w_toString/Customer.java
@@ -0,0 +1,27 @@
+
+public class Customer {
+	//instance var
+	
+		// Name of customer.
+		 
+		String name;
+		
+		//constructor
+		
+		public Customer(String name) {
+			this.name = name;
+		}
+		
+		
+		 // Returns name of customer.
+
+		public String toString() {
+			return this.name; // returns name of customer to be printed by Java
+		}
+		
+		public static void main(String[] args) {
+			Customer c = new Customer("Khushii");
+			System.out.println("c = " + c); //this will try to convert customer c to a String by calling the toString method
+		}
+		
+}
